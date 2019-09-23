ca673cbf3a9992766ce733332e9d73d75b2e3b12
TheLegion007 <manisksing@gmail.com>
2019-09-24 02:01:47 +0530

    fun made string added

--- app/src/main/java/com/manish/gittu1/MainActivity.java
+++ app/src/main/java/com/manish/gittu1/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int a = 123;
+    }
+    void func() {
+        String S ="kidhar ja rah bhai";
     }
 }
